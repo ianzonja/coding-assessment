d1a5e98277773bfa0efdcf5e934809c471d2d764
Ian <ianzonja@gmail.com>
2024-01-21 07:00:24 +0100

    some responsivity improvements

--- src/app/modules/core/layout/app-layout/app-layout.component.ts
+++ src/app/modules/core/layout/app-layout/app-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LayoutRoutingModule } from '../layout-routing.module';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-app-layout',
   templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.css']
+  styleUrls: ['./app-layout.component.scss'],
+  providers: [MessageService]
 })
 export class AppLayoutComponent {
 
